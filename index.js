cc75f766d8a799352000149ee1e5e4a251c315d4
Mike Zahalan <MikeZahalan@GMail.COM>
2024-04-21 00:35:11 -0700

    Added connection to the mud

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-const express = require('express');
-const WebSocket = require('ws');
-const http = require('http');
+const express = require('express')
+const WebSocket = require('ws')
+const http = require('http')
+const { Telnet } = require('telnet-client') 
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 const PORT = 9081
 
+const PARAMS = {
+    host: '127.0.0.1',
+    port: 4000,
+    disableLogin: true,
+    shellPrompt: null,
+    timeout: 10500
+}
+
 wss.on('connection', (ws) => {
-    console.log('Client Connected');
+    console.log('Client Connected - setting up mud connect');
+
+    let mudCon = new Telnet()
+    
+    mudCon.on('connect', data => {
+        ws.send('connected')
+    })
+
+    mudCon.on('data', data => {
+        ws.send(data.toString().trim())
+    })
+
+    mudCon.on('timeout', () => {
+        // just wait longer
+    })
+
+    mudCon.on('close', () => {
+        ws.send('connection closed')
+        ws.close()
+    })
+
+    mudCon.connect(PARAMS)
+
     ws.on('message', (message) => {
-        console.log(`Received: ${message}`);
-        ws.send(`You sent: ${message}`);
+        mudCon.send(message)
     });
 
     ws.on('close', () => {
-        console.log('Client disconnected');
+        mudCon.close()
     });
 });
 
