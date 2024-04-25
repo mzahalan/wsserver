8d2a38195b10a30d5a1b89acee285b12ac4bd34d
Mike Zahalan <mzahalan@amazon.com>
2024-04-25 12:32:00 -0700

    Improved color handling and white space formatting

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require('express')
 const WebSocket = require('ws')
 const http = require('http')
 const { Telnet } = require('telnet-client') 
+const Convert = require('ansi-to-html')
+const CONVERTER = new Convert({newline:true});
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 const PORT = 9081
+const STRIP_CHARS=[0x0A]
 
 const PARAMS = {
     host: '127.0.0.1',
     port: 4000,
     disableLogin: true,
     shellPrompt: null,
-    timeout: 10500
+    timeout: 10500,
+    stripControls: true
 }
 
 wss.on('connection', (ws) => {
@@ -26,7 +30,21 @@ wss.on('connection', (ws) => {
     })
 
     mudCon.on('data', data => {
-        ws.send(data.toString().trim())
+        let sendMsg = ""
+
+        for(const b of data.values()) {
+            if(b > 6 && b < 128 && !STRIP_CHARS.includes(b)){
+                if(b == 0x09) {
+                    sendMsg += '\t'
+                } else if (b == 0x20) { 
+                    sendMsg += '&nbsp;'
+                } else {
+                    sendMsg += String.fromCharCode(b)
+                }
+            }
+        }
+
+        ws.send(CONVERTER.toHtml(sendMsg))
     })
 
     mudCon.on('timeout', () => {
@@ -45,7 +63,7 @@ wss.on('connection', (ws) => {
     });
 
     ws.on('close', () => {
-        mudCon.close()
+        mudCon.end()
     });
 });
 
